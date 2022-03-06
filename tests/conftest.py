9d3f27126d322ece3d89d9af7561c60cb5d2c0ea
Lylybs <71543467+Lylybs@users.noreply.github.com>
2022-03-06 16:09:39 +0100

    update test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 import pytest
+import website.toxic_app as ta
 from website.toxic_app import createapp
 
 @pytest.fixture
 def client():
-    app = createapp()
+    app = ta.createapp()
     with app.test_client() as client:
         yield client
